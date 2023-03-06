26f83f4b3b18d063cd86651636d8a542da1a4fd1
ayu-lif3 <114067713+ayu-lif3@users.noreply.github.com>
2023-03-06 18:37:46 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,87 +1,103 @@
-struct PCB{
-    PCB *next;
-    // All the other details of a process that a PCB contains
-    .
-    .
-    .
-    .
-    .
+#include <bits/stdc++.h>
+using namespace std;
+
+//the blocked queue stores the process id of those processes which are blocked
+queue<int> blocked_queue;
+//the ready queue stores the process id of those processes which are run
+queue<int> ready_queue;
+
+void stuff(){
+    //to initialise the ready queue with some dummy values
+    ready_queue.push(1);
+    ready_queue.push(2);
+    ready_queue.push(3);
+    ready_queue.push(4);
+    ready_queue.push(5);
+    ready_queue.push(6);
+    ready_queue.push(7);
+    ready_queue.push(8);
+    ready_queue.push(9);
+    ready_queue.push(10);
 }
 
-struct Queue{
-
-    ProcessControlBlock *front, *rear;
-
-    Queue(){
-        front = nullptr;
-        rear = nullptr;
-    }
-
-    // Function that pushes a PCB at the rear end of the queue
-    void push(int pID){
-        ProcessControlBlock *pcb = new ProcessControlBlock();
-        pcb->ID = pID;
-        if(rear == nullptr){
-            front = pcb;
-            rear = pcb;
-        }else{
-            rear->next = pcb;
-            rear = pcb;
-        }
-    }
-
-    // Function that pops a PCB from the front end of the queue
-    ProcessControlBlock *pop(){
-        if(front == nullptr){
-            return nullptr;
-        }else{
-            ProcessControlBlock *pcb = front;
-            front = front->next;
-            if(front == nullptr){
-                rear = nullptr;
-            }
-            return pcb;
-        }
-    }
-
-};
-
-
 struct Semaphore{
-
-    int semaphore = 1; // The actual value of the semaphore
-    Semaphore(){
-        semaphore = 1;
+    int val = 1;
+    Semaphore(int n){
+        val = n;
     }
-    Semaphore(int s){
-        semaphore = s;
-    }
-
-    // A queue to store the waiting processes
-    Queue *FIFO = new Queue();
-
-    // A function to implement the 'wait' functionality of a semaphore
-    void wait(int pID){
-        semaphore--;
-
-        // If all resources are busy, push the process into the waiting queue and 'block' it
-        if(semaphore < 0){
-            FIFO->push(pID);
-            ProcessControlBlock *pcb = FIFO->rear;
-            block(pcb);
-        }
-    }
-
-    // A function to implement the 'signal' functionality of a semaphore
-    void signal(){
-        semaphore++;
-
-        // If there are any processes waiting for execution, pop from the waiting queue and wake the process up
-        if(semaphore <= 0){
-            ProcessControlBlock *pcb = FIFO->pop();
-            wakeup(pcb);
-        }
-    }
-
 };
+
+void wakeUp(int pid){
+    //to wake up the process with the process id pid
+    ready_queue.push(pid);
+}
+
+void signal(Semaphore *s){
+    //signal function, wakes up one process from the blocked queue
+    s->val = s->val +1;
+    if(s->val<=0){
+        if(!blocked_queue.empty()){
+            wakeUp(blocked_queue.pop());
+        }
+    }
+}
+
+void wait(Semaphore *s, int p){
+    //puts processes in blocked queue depending on the value of semaphore
+    s->val = s->val -1;
+    if(s->val<0){
+        blocked_queue.push(p);
+    }
+}
+
+Semaphore* wrt = new Semaphore(1);
+Semaphore* in = new Semaphore(1);
+Semaphore* mx = new Semaphore(1);
+int c = 0;
+
+void reader(int p){
+    wait(in, p);
+    wait(mx, p);
+    c++;
+    if(c==1){
+        wait(wrt, p);
+    }
+    signal(mx);
+    signal(in);
+    //critical section, i.e. the part where reading happens
+    //read
+    c--;
+    if(c==0){
+        signal(wrt);
+    }
+    signal(mx);
+}
+
+void starvefreereader(){
+    do{
+        int pid = 0;
+        if(!ready_queue.empty()){
+            pid = ready_queue.pop();
+        }
+        reader(pid);
+    }while(true);
+}
+
+void writer(int p){
+    wait(in, p);
+    wait(wrt, p);
+    //critical section, i.e the part where writing happens
+    signal(wrt);
+    signal(in);
+}
+
+void starvefreewriter(){
+    do{
+        int pid=0;
+        if(!ready_queue.empty()){
+            pid = ready_queue.pop();
+        }
+        writer(pid);
+    }while(true);
+}
 
